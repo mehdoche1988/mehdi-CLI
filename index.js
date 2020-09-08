b91880cf7f77174dcede0da8b12825636a471829
becode <mehdi.semlali.1988@gmail.com>
2020-09-08 11:00:50 +0200

    ok for belgium

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Hello, Node.JS!");
+#!/usr/bin/env node
+
+
+// CONST. AND VAR.
+const axios = require('axios');
+const countryList = require('country-list');
+const chalk = require('chalk');
+const boxen = require('boxen');
+const ora = require('ora');
+var figlet = require('figlet'); 
+const { overwrite, getName } = require('country-list');
+
+
+
+async function axiosGetHoliday() {
+    try {
+        return await axios.get("https://date.nager.at/api/v2/publicholidays/2020/BE");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+axiosGetHoliday()
+    .then(holidays => {
+
+        for (let i = 0; i < holidays['data'].length; i++) {
+
+            console.log((holidays['data'][i]["date"]) + ": " + (holidays['data'][i]["name"]));
+            
+        }
+    })
+    .catch(error => consgitole.error(error));
+
+
+figlet('Product by', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+figlet('Mehdoche1988', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
